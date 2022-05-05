eca67fa1095aafa3a3aad455a3c36477e3eb64f1
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-05-05 22:18:04 +0100

    Removed redundant controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,16 +1,5 @@
 import {PingController} from './ping';
-import { ProdutoController } from './produto';
-import { EntradaProdutoController } from './entradaProduto';
-import { SaidaProdutoController } from './saidaProduto';
-import { EstoqueController } from './estoque';
-import { UsuarioController } from './usuario';
-
 
 export const CONTROLLERS = [
-    new PingController(),
-    new ProdutoController(),
-    new EntradaProdutoController(),
-    new SaidaProdutoController(),
-    new EstoqueController(),
-    new UsuarioController()
+    new PingController()
 ];
