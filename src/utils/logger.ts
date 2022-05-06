fc85f297058ce5e27b788c6c99047a43360229ad
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-05-06 18:56:00 +0100

    Added Logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,28 @@
+import * as winston from 'winston'
+
+const logger: winston.Logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+        winston.format.timestamp({
+            format: 'YYYY-MM-DD HH:mm:ss'
+        }),
+        winston.format.errors({ stack: true }),
+        winston.format.splat(),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'sadaqah' },
+    transports: []
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple()
+        )
+    }));
+} else {
+    logger.add(new winston.transports.Console());
+}
+
+export = logger
