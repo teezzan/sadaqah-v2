b80d836f51160f00ecd04b7dcd7a807b9f629d0f
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-05-08 15:47:13 +0100

    Added Restify Error library

--- src/middleware/Auth.ts
+++ src/middleware/Auth.ts
@@ -1,19 +1,24 @@
 import admin = require("../utils/firebase");
-import errors from "restify-errors";
+import * as errors from "restify-errors";
 import { Next, Response } from "restify";
 import { RequestWithContext } from "../types/restify";
 
-export async function authentication(req: RequestWithContext, res: Response, next: Next) {
+export async function authentication(
+  req: RequestWithContext,
+  res: Response,
+  next: Next
+) {
   const token = req.headers?.authorization?.split(" ")[1];
 
   if (!token) {
-    res.send(
-      new errors.UnauthorizedError(
-        { statusCode: 401 },
-        "Bearer Token is missing"
+    return next(
+      res.send(
+        new errors.UnauthorizedError(
+          { statusCode: 401 },
+          "Bearer Token is missing"
+        )
       )
     );
-    return next(false);
   }
 
   const decodeValue = await admin.auth().verifyIdToken(token);
