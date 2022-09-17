35d63f1e5522aeeea225131febf7e61fadb10fee
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-09-17 17:02:49 +0100

    Pushed some db changes up from tweed's branch

---
+++ src/database/models/user.ts
@@ -0,0 +1,44 @@
+import {
+  Model,
+  Table,
+  Column,
+  DataType,
+  HasMany,
+  CreatedAt,
+  UpdatedAt,
+} from "sequelize-typescript";
+
+const { QueryTypes } = require("sequelize");
+
+@Table({
+  tableName: "users",
+  updatedAt: "updated",
+  createdAt: "created",
+  deletedAt: "deleted",
+  paranoid: true,
+})
+export class User extends Model {
+  @Column({
+    type: DataType.TEXT,
+    field: "name",
+  })
+  name: string;
+
+  @Column({
+    type: DataType.TEXT,
+    field: "email",
+  })
+  email: string;
+
+  @Column({
+    type: DataType.TEXT,
+    field: "avatar",
+  })
+  avatar: string;
+
+  @Column({
+    type: DataType.TEXT,
+    field: "external_user_id",
+  })
+  externalUserId: string;
+}
