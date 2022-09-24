17ca7654e6fe9ff19f699c89580127c80d198dbf
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2022-09-24 12:58:00 +0100

    Updated tests

---
+++ src/tests/controllers/groupController.test.ts
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import server from "../../server";
+import * as request from "supertest";
+import { createTestUser } from "../helpers/createUserHelper";
+import { DatabaseProvider } from "../../database";
+
+let user;
+let authToken: string;
+describe("Group controller methods", () => {
+  beforeAll(async () => {
+    const dbConn = DatabaseProvider.getConnection();
+    await dbConn.sync();
+    [user, authToken] = await createTestUser({});
+    await request(server)
+      .get("/api/user/login")
+      .set({
+        Authorization: `Bearer ${authToken}`,
+      });
+  });
+  afterAll(() => {
+    server.close();
+  });
+
+  test("Should create a group", async () => {
+    const groupName = "RandoGroup";
+
+    const response = await request(server)
+      .post("/api/group/create")
+      .set({
+        Authorization: `Bearer ${authToken}`,
+      })
+      .send({
+        name: groupName,
+      });
+    expect(response.status).toBe(201);
+    expect(response.body.name).toBe(groupName);
+    expect(response.body.id).not.toBeFalsy();
+  });
+
+  test("Should fail to create a group with duplicate name", async () => {
+    const groupName = "RandoGroup";
+    const response = await request(server)
+      .post("/api/group/create")
+      .set({
+        Authorization: `Bearer ${authToken}`,
+      })
+      .send({
+        name: groupName,
+      });
+    expect(response.status).not.toBe(201);
+  });
+});
